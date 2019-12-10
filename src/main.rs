ab9c07135b9de22c73f89fc666c5816284017595
Andreas Monitzer <andreas@monitzer.com>
2019-12-10 21:29:11 +0100

    Implemented demo code for zbox failing for file URLs.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use url::Url;
+use zbox::RepoOpener;
+
+fn main() {
+    let mut path = std::env::current_exe().unwrap();
+    path.pop();
+    path.push("test-fs");
+
+    if let Err(err) = std::fs::remove_dir_all(&path) {
+        if err.kind() != std::io::ErrorKind::NotFound {
+            panic!("Unable to remove archive file: {}", err);
+        }
+    }
+
+    let url = Url::from_file_path(&path).unwrap();
+
+    eprintln!("Opening URL: {}", &url);
+    RepoOpener::new()
+        .create_new(true)
+        .open(&format!("{}", url), "abcdef")
+        .expect("opening archive failed");
+
+    eprintln!("Success!");
+}
